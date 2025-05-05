00e2395e7e4ec018d21f849f85a87a596ef90a82
Kabir Asani <iamkabirasani@gmail.com>
2025-05-05 10:49:35 +0530

    Add database configuration to betterAuthClient using Prisma adapter

--- src/integrations/better-auth/index.ts
+++ src/integrations/better-auth/index.ts
@@ -1,9 +1,14 @@
 import { betterAuth } from "better-auth";
 import { serverUrl } from "../../utils/environment";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prismaClient } from "../prisma";
 
 export const betterAuthClient = betterAuth({
   baseURL: serverUrl,
   basePath: "/authentications",
+  database: prismaAdapter(prismaClient, {
+    provider: "postgresql",
+  }),
   user: {
     modelName: "User",
   },
